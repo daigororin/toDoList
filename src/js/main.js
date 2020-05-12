5a925ede376d673efe5ff5d2ebf60102eab4e9de
daigororin <60973962+daigororin@users.noreply.github.com>
2020-05-12 16:42:37 +0900

    toDoVer1

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 const add = document.getElementById("add");
 const  textElement = document.getElementById("text");
+
 add.addEventListener("click",  () => {
-  console.log(textElement.value);
+  const li = document.createElement("li");
+  li.textContent = textElement.value;
+  document.getElementById("ul").appendChild(li);
+  // console.log(li);
 });
